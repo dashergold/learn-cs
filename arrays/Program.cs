187ce94c467b3089c93d35624fe72ab43132071e
dashergold <axel.s.kallen@gmail.com>
2021-04-27 12:50:03 +0200

    arrays

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -6,7 +6,18 @@ namespace arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new string[3];
+            a[0] = "hej";
+            a[1] = "din";
+            a[2] = "tjockskalle";
+            var s = string.Join(" ", a);
+            var a2 = s.Split(" ");
+            Console.WriteLine(s);
+            a2[2] = "dumbom";
+            var s2 = string.Join(",", a2);
+            Console.WriteLine(s2);
+
+            Console.WriteLine(a2[1]);
         }
     }
 }
