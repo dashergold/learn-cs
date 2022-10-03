7d0495827dbbb84ee6c563ab7e99c1bb0dd00cff
dashergold <axel.s.kallen@gmail.com>
2022-10-03 14:42:43 +0200

    read number

--- code translator/tokenizer.cs
+++ code translator/tokenizer.cs
@@ -10,7 +10,8 @@ namespace code_translator
 {
     internal class Tokenizer
     {
-        enum Token {
+        enum Token
+        {
             WHILE = 1,
             IF = 2,
             ELSE = 3,
@@ -39,33 +40,66 @@ namespace code_translator
     };
 
 
-        int discardComment(string text, int i) {
-            while( i < text.Length && text[ i] != '\n'){
+        int discardComment(string text, int i)
+        {
+            while (i < text.Length && text[i] != '\n')
+            {
                 ++i;
 
             }
             return i + 1;
 
         }
-
-        public void tokenize(string text)
+        (int, int) tokenizeNumber(string text, int i)
         {
-            int i = 0;
 
+
+            int n = 0;
             while (i < text.Length)
             {
-                char c = text[i];
+                var c = text[i];
+
+                if (!char.IsDigit(c))
+
+                {
+                    return (n, i);
+
+                }
+                var d = c - 48;
+                n = n * 10 + d;
+                ++i;
+            }return (n, i);
+        } 
+
+
+                public void tokenize(string text)
+                {
+                    int i = 0;
+
+                    while (i < text.Length)
+                    {
+                        char c = text[i];
                 if (c == '#')
                 {
                     //discard comment
                     i = discardComment(text, i);
+                }
+                else if (char.IsDigit(c))
+                {
+                    //read number
+                    int a;
+                    (a, i) = tokenizeNumber (text, i);
+                    Console.Write(Token.NUMBER);
+                    Console.Write(" ");
+
+                    
                 }
                 else
                 {
                     Console.Write(c);
                     ++i;
                 }
+                    }
+                }
             }
         }
-    }   
-}
