edd21e58cb2e1f15a0581258ca25fbc80f7e7f9a
dashergold <axel.s.kallen@gmail.com>
2023-02-03 13:24:24 +0100

    token file reader class and test

---
+++ code translator/TokenFileReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_translator
+{
+    public class TokenFileReader
+    {
+        public TokenFileReader(TextReader rdr)
+        {
+            this.Dictionary = new Dictionary<string, TokenType>();
+            
+            var line = rdr.ReadLine();
+            while (line != null)
+            {
+                processLine(line);
+                line = rdr.ReadLine();
+            }
+
+        }
+        public TokenFileReader(string fileContents) : this(new StringReader(fileContents))
+        {
+        }
+
+        private void processLine(string line)
+        {
+            if (line.Length== 0)
+            {
+                return;
+            }
+            var fields = line.Split(" ");
+            if (fields.Length != 2)
+            {
+                return;
+            }
+            var tt = Enum.Parse<TokenType>(fields[1]);
+            this.Dictionary.Add(fields[0],tt);
+        }
+
+        public Dictionary<string, TokenType> Dictionary { get; set; }
+    }
+}
---
+++ code translator/UnitTest/TokenFileReaderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using code_translator;
+
+namespace UnitTest
+{
+    internal class TokenFileReaderTests
+    {
+        [Test]
+        public void LoadFileContents()
+        {
+            var fileContents =
+@"
+
+
+om IF
+medan WHILE
+
+";
+            var tfr = new TokenFileReader(fileContents);
+            var d = tfr.Dictionary;
+            Assert.That(d.Count, Is.EqualTo(2));
+            Assert.That(d["om"], Is.EqualTo(TokenType.IF));
+            Assert.That(d["medan"], Is.EqualTo(TokenType.WHILE));
+        }
+        
+    }
+}
